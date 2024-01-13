18c8563f9784f1b82ea42c319f18a69fafa4a7fd
dongjun-Yi <djyi0223@gmail.com>
2024-01-13 16:15:25 +0900

    Mockito 애네테이션을 활용한 MailSend 테스트 생성

--- cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -1,7 +1,47 @@
 package sample.cafekiosk.spring.api.service.mail;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sample.cafekiosk.spring.client.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.spring.domain.history.mail.MailSendRepository;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendRepository mailSendRepository;
+
+    @InjectMocks
+    private MailService mailService;
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail() {
+        //given
+        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(true);
+        
+        //when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        //then
+        assertThat(result).isTrue();
+        verify(mailSendRepository, times(1)).save(any(MailSendHistory.class));
+    }
 
 }
