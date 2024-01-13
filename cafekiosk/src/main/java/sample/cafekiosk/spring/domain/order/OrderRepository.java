facf55ea69d4152dd7355b1797b3c5e093d569cb
dongjun-Yi <djyi0223@gmail.com>
2024-01-13 15:48:16 +0900

    @Query에 @Param 애너테이션 추가

--- cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/OrderRepository.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/OrderRepository.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.domain.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -11,5 +12,5 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("select o from Order o where o.registeredDateTime >= :startDateTime and o.registeredDateTime < :endDateTime " +
             "and o.orderStatus = :orderStatus")
-    List<Order> findOrdersBy(LocalDateTime startDateTime, LocalDateTime endDateTime, OrderStatus orderStatus);
+    List<Order> findOrdersBy(@Param("startDateTime") LocalDateTime startDateTime, @Param("endDateTime") LocalDateTime endDateTime, @Param("orderStatus") OrderStatus orderStatus);
 }
